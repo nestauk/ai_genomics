4e54e20101032eb74ee85f39b6370f19ea51b268
India Kerle <india.kerle@nesta.org.uk>
2022-07-10 17:52:33 +0100

    updated to also extract ipc codes prettier

--- ai_genomics/pipeline/patent_data/get_genomics_codes.py
+++ ai_genomics/pipeline/patent_data/get_genomics_codes.py
@@ -1,4 +1,4 @@
-"""Simple script to extract cpc codes related to genomics via
+"""Simple script to extract cpc and ipc codes related to genomics via
 exact match.
 
 To run: python get_genomics_codes.py
@@ -19,7 +19,7 @@ from ai_genomics.getters.data_getters import (
 GENOMICS_KEYWORDS = ["genome", "dna", "gene", "genetic"]
 
 
-def clean_cpc_code(cpc_code_text):
+def clean_class_code(cpc_code_text):
     """Cleans CPC definitions by:
             - lowercasing;
             - replacing values;
@@ -38,23 +38,47 @@ def clean_cpc_code(cpc_code_text):
     )
 
 
+def get_genomics_classification_codes(class_codes: list) -> dict:
+    """Get genomics-related classification codes.
+    
+    Inputs:
+        class_codes (list): List of patent classification codes.
+    
+    Outputs:
+        genomics_codes (dict): Dict of relevant genomics-related
+        classification codes.
+    """
+    genomics_codes = dict()
+    for code in class_codes:
+        code_clean = clean_class_code(code[1])
+        if any([code in code_clean for code in GENOMICS_KEYWORDS]):
+            genomics_codes[code[0]] = " ".join(code_clean)
+    return genomics_codes
+
+
 if __name__ == "__main__":
 
-    # load data
-    cpc_code_files = get_s3_dir_files(s3, bucket_name, config["cpc_codes_path"])
+    class_code_files = get_s3_dir_files(s3, bucket_name, "inputs/patent_data/")
 
     cpc_codes = []
-    for cpc_code_file in cpc_code_files[1:]:
-        cpc_codes.extend(load_s3_data(bucket_name, cpc_code_file))
+    ipc_codes = []
+    for class_code in class_code_files:
+        if "txt" in class_code:
+            if "cpc" in class_code:
+                cpc_codes.extend(load_s3_data(bucket_name, class_code))
+            elif "ipc" in class_code:
+                ipc_codes.extend(load_s3_data(bucket_name, class_code))
 
-    # remove empty rows
-    cpc_codes_not_empty = [cpc_code for cpc_code in cpc_codes if len(cpc_code) == 3]
+    cpc_codes_not_empty, ipc_codes_not_empty = (
+        [[i[0], i[2]] for i in cpc_codes if len(i) == 3],
+        [[i[0], i[1]] for i in ipc_codes if len(i) == 2],
+    )
 
-    # exact match based on genomics key words
-    cpc_genomics_codes = dict()
-    for cpc_code in cpc_codes_not_empty:
-        cpc_code_clean = clean_cpc_code(cpc_code[2])
-        if any([code in cpc_code_clean for code in GENOMICS_KEYWORDS]):
-            cpc_genomics_codes[cpc_code[0]] = " ".join(cpc_code_clean)
+    genomics_codes_dict = dict()
+    genomics_codes_dict["cpc"] = get_genomics_classification_codes(cpc_codes_not_empty)
+    genomics_codes_dict["ipc"] = get_genomics_classification_codes(ipc_codes_not_empty)
+
     # save list of codes to s3
-    save_to_s3(s3, bucket_name, cpc_genomics_codes, config["genomics_cpc_codes_file"])
+    save_to_s3(
+        s3, bucket_name, genomics_codes_dict, config["genomics_class_codes_file"]
+    )
