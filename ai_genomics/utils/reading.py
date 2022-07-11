be30dc3fa57efa125c07a9f3e4ba078d708fe444
Juan Mateos-Garcia <Juan.mateos-garcia@nesta.org.uk>
2022-07-11 14:18:21 +0100

    Update ai_genomics/utils/reading.py
    Co-authored-by: George Richardson <georgerichardson@posteo.net>

--- ai_genomics/utils/reading.py
+++ ai_genomics/utils/reading.py
@@ -5,6 +5,6 @@ def read_json(data: Union[pathlib.Path, str]) -> dict:
     """
     Reads the json file and returns the data
     """
-    with open(f"{data}") as json_file:
+    with open(data) as json_file:
         data = json.load(json_file)
     return data
