4fe6045d03b8bdfd81ff37560fcd4ed5c25d9ecd
Juan Mateos-Garcia <juan.mateos-garcia@nesta.org.uk>
2022-07-12 13:29:10 +0100

    fixes and additions to existing scripts

--- ai_genomics/getters/openalex.py
+++ ai_genomics/getters/openalex.py
@@ -1,8 +1,11 @@
+import json
+from typing import List, Dict, Any
+
 from ai_genomics.utils.reading import read_json
 from ai_genomics import PROJECT_DIR
 
 
-def get_openalex_works() -> list:
+def get_openalex_works() -> List[Dict[Any, Any]]:
     """Reads OpenAlex works (papers)
 
     Returns:
@@ -35,3 +38,22 @@ def get_openalex_concepts() -> list:
     """
 
     return read_json(f"{PROJECT_DIR}/inputs/openalex/concepts.json")
+
+
+def get_year_summary(
+    subject: str = "artificial_intelligence",
+) -> Dict[int, Dict[str, Any]]:
+    """Reads a summary of results for a year
+
+    Args:
+        subject: The subject of the docs (AI or genetics)
+
+    Returns:
+        A list where every element is a dict with results for a year
+
+    """
+
+    with open(
+        f"{PROJECT_DIR}/outputs/data/openalex/{subject}_year_summaries.json", "r"
+    ) as infile:
+        return json.load(infile)
