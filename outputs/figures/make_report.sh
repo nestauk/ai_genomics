0e223abe10a2af545cff74faab1e10d6b83151b4
Juan Mateos-Garcia <juan.mateos-garcia@nesta.org.uk>
2022-08-04 16:14:10 +0100

    added bash script

--- outputs/figures/make_report.sh
+++ outputs/figures/make_report.sh
@@ -1,1 +1,1 @@
-pandoc --filter=pandoc-crossref -s executive_summary.md section_introduction.md section_data_sources.md -o 2022_august_03_ai_genomics.pdf --number-sections
+pandoc --filter=pandoc-crossref -s executive_summary.md section_introduction.md section_data_sources.md -o $1_ai_genomics.pdf --number-sections
