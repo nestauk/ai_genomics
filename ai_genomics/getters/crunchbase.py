56e03375916576b05847eec0612e840748985a5c
Jack Rasala <jack.rasala@nesta.org.uk>
2022-08-23 00:10:40 +0900

    Add getter for ai genomics crunchbase org ids

---
+++ ai_genomics/getters/crunchbase.py
@@ -0,0 +1,17 @@
+from ai_genomics import PROJECT_DIR, logger
+import pandas as pd
+
+
+def get_ai_genomics_crunchbase_org_ids() -> pd.DataFrame:
+    """Returns dataframe of AI and Genomics crunchbase organisation ids"""
+    try:
+        return pd.read_csv(
+            PROJECT_DIR / "inputs/data/crunchbase/ai_genomics_org_ids.csv",
+            index_col=0,
+            names=["cb_org_id"],
+            skiprows=[0],
+        )
+    except FileNotFoundError:
+        logger.error(
+            "FileNotFoundError: To create the file, run ai_genomics/analysis/crunchbase_definitions.py"
+        )
