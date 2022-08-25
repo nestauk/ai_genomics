a5215aa4ef6cfdb191d150f1d0511b5cfedb39f2
Jack Rasala <jack.rasala@nesta.org.uk>
2022-08-26 00:47:04 +0900

    Remove unused import and fix docstring

--- ai_genomics/utils/crunchbase.py
+++ ai_genomics/utils/crunchbase.py
@@ -5,9 +5,6 @@ import pandas as pd
 from toolz import pipe
 
 
-from ai_genomics import config
-
-
 def fetch_crunchbase(
     table_name: str,
 ) -> pd.DataFrame:
@@ -25,7 +22,7 @@ def fetch_crunchbase(
 
 
 def parse_s3_table(s3_object) -> pd.DataFrame:
-    """PParses an s3 object into a pandas dataframe"""
+    """Parses an s3 object into a pandas dataframe"""
     return pipe(
         s3_object,
         lambda _object: _object["Body"].read().decode("utf-8"),
