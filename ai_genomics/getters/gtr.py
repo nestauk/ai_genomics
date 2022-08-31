d598159299a8c0158db400b341dfb1af971c684e
Jack Rasala <jack.rasala@nesta.org.uk>
2022-08-31 12:32:47 +0900

    Update gtr getter variable data_type to entity_type and add ValueError

--- ai_genomics/getters/gtr.py
+++ ai_genomics/getters/gtr.py
@@ -5,18 +5,23 @@ import re
 GTR_INPUTS_DIR = PROJECT_DIR / "inputs/data/gtr"
 
 
-def get_ai_genomics_gtr_data(data_type: str) -> pd.DataFrame:
+def get_ai_genomics_gtr_data(entity_type: str) -> pd.DataFrame:
     """Get AI and Genomics GtR data
 
     Args:
-        data_type: "projects" or "publications"
+        entity_type: "projects" or "publications"
 
     Returns:
         Dataframe of AI and Genomics GtR projects or publications
     """
+    if entity_type not in ["projects", "publications"]:
+        raise ValueError(
+            "ValueError: entity_type should be 'projects' or 'publications'"
+        )
+
     try:
         return pd.read_json(
-            GTR_INPUTS_DIR / f"gtr_ai_genomics_{data_type}.json"
+            GTR_INPUTS_DIR / f"gtr_ai_genomics_{entity_type}.json"
         ).rename(columns=lambda x: re.sub(r"(?<!^)(?=[A-Z])", "_", x).lower())
     except ValueError as e:
         logger.error(
