31be6a8a6969860cae077f59ad7e2fbbc4af392a
Juan Mateos-Garcia <juan.mateos-garcia@nesta.org.uk>
2022-09-09 13:14:00 +0100

    updated getters to fetch crunchbase table

--- ai_genomics/getters/crunchbase.py
+++ ai_genomics/getters/crunchbase.py
@@ -1,5 +1,13 @@
 from ai_genomics import PROJECT_DIR, logger
+import os
 import pandas as pd
+
+from ai_genomics.pipeline.crunchbase_data.crunchbase_data import (
+    CB_COMP_PATH,
+    CB_COMP_NAME,
+)
+
+from ai_genomics.utils.reading import fetch_s3
 
 
 def get_ai_genomics_crunchbase_org_ids() -> pd.DataFrame:
@@ -13,3 +21,17 @@ def get_ai_genomics_crunchbase_org_ids() -> pd.DataFrame:
             "FileNotFoundError: To create the missing file, run ai_genomics/analysis/crunchbase_definitions.py"
         )
         raise e
+
+
+def get_ai_genomics_crunchbase_companies(local: bool = True) -> pd.DataFrame:
+    """Reads a table with information about AI, Genomics or AI and genomics companies
+
+    Args:
+        local: If True, read from local file. If False, read from s3 bucket.
+
+    """
+
+    if local:
+        return pd.read_csv(CB_COMP_PATH)
+    else:
+        return fetch_s3(f"outputs/crunchbase/{CB_COMP_NAME}")
