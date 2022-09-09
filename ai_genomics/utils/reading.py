7789368cc29b38de7637778204b3844484f33e6a
Juan Mateos-Garcia <juan.mateos-garcia@nesta.org.uk>
2022-09-09 13:14:28 +0100

    added fetch_s3 function

--- ai_genomics/utils/reading.py
+++ ai_genomics/utils/reading.py
@@ -1,6 +1,8 @@
 import json
 import pathlib
 from typing import Union, Dict, List, Any
+import pandas as pd
+import boto3
 
 
 def read_json(data: Union[pathlib.Path, str]) -> List[Dict]:
@@ -9,3 +11,13 @@ def read_json(data: Union[pathlib.Path, str]) -> List[Dict]:
     """
     with open(data) as json_file:
         return json.load(json_file)
+
+
+def fetch_s3(s3_path) -> Union[pd.DataFrame, Dict]:
+    """
+    Fetches the s3 file and returns the data
+    """
+    s3 = boto3.resource("s3")
+    bucket = s3.Bucket("ai-genomics")
+    obj = bucket.Object(s3_path)
+    return pd.read_csv(obj.get()["Body"])
