6091dda527e76dc486a8bd7d41c029ebfe601635
India Kerle <india.kerle@nesta.org.uk>
2022-09-13 12:09:12 +0100

    modify docstrings for accurate sampling

--- ai_genomics/getters/patents.py
+++ ai_genomics/getters/patents.py
@@ -66,7 +66,7 @@ def get_ai_genomics_cpc_codes() -> Mapping[str, Mapping[str, Union[str, str]]]:
 
 
 def get_ai_genomics_ipc_codes() -> Mapping[str, Mapping[str, Union[str, str]]]:
-    """From S3 loads AI in genomics ipc codes without Google BigQuery ipc code formatting"""
+    """From S3 loads AI in genomics ipc codes WITHOUT Google BigQuery ipc code formatting"""
     return load_s3_data(
         bucket_name, "outputs/patent_data/class_codes/ipc_with_definitions.json"
     )
@@ -75,7 +75,7 @@ def get_ai_genomics_ipc_codes() -> Mapping[str, Mapping[str, Union[str, str]]]:
 def get_ai_genomics_ipc_codes_formatted() -> Mapping[
     str, Mapping[str, Union[str, str]]
 ]:
-    """From S3 loads AI in genomics ipc codes with Google BigQuery ipc code formatting"""
+    """From S3 loads AI in genomics ipc codes WITH Google BigQuery ipc code formatting"""
     return load_s3_data(
         bucket_name,
         "outputs/patent_data/class_codes/ipc_formatted_with_definitions.json",
