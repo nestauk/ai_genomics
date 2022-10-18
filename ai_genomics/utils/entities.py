c0e1396c9656bb8f652b780a2b07e3f8ce78d601
George Richardson <georgerichardson@posteo.net>
2022-10-18 16:34:00 +0100

    Change entity filtering with float to use percentile

--- ai_genomics/utils/entities.py
+++ ai_genomics/utils/entities.py
@@ -22,27 +22,29 @@ def filter_entities(
             for a set of documents without scores.
         min_entity_freq (Optional[Union[int, float]], optional): The minimum
             frequency for an entity. Any entities with a frequency below this
-            will be filtered. Defaults to None.
+            will be filtered. If a float is passed, the mininum frequency will
+            be the value at this percentile in the frequency distribution.
+            Defaults to None.
         max_entity_freq (Optional[Union[int, float]], optional): The maximum
             frequency for an entity. Any entities with a frequency above this
-            will be filtered. Defaults to None.
+            will be filtered. If a float is passed, the maximum frequency will
+            be the value at this percentile in the frequency distribution.
+            Defaults to None.
 
     Returns:
         Mapping[str, Mapping[str, Union[str, str]]]: Filtered entities.
     """
     entity_freqs = Counter(chain(*entities.values()))
-    entity_freq_low = min(entity_freqs.values())
-    entity_freq_high = max(entity_freqs.values())
 
     if type(min_entity_freq) == float:
-        min_entity_freq = np.round(min_entity_freq * entity_freq_high)
+        min_entity_freq = np.percentile(list(entity_freqs.values()), min_entity_freq)
     elif min_entity_freq is None:
-        min_entity_freq = entity_freq_low
+        min_entity_freq = min(entity_freqs.values())
 
     if type(max_entity_freq) == float:
-        max_entity_freq = np.round(max_entity_freq * entity_freq_high)
+        max_entity_freq = np.percentile(list(entity_freqs.values()), max_entity_freq)
     elif max_entity_freq is None:
-        max_entity_freq = entity_freq_high
+        max_entity_freq = max(entity_freqs.values())
 
     return {
         k: [e for e in v if min_entity_freq <= entity_freqs[e] <= max_entity_freq]
