7440b8158ee38b69ba7d1cb1ecc8def186bf1d08
George Richardson <georgerichardson@posteo.net>
2022-10-18 14:55:54 +0100

    Add utils and pipeline for entitiy clustering

---
+++ ai_genomics/pipeline/entity_cluster/__init__.py
@@ -0,0 +1,83 @@
+from itertools import chain
+import numpy as np
+import pandas as pd
+from sklearn.cluster import KMeans
+
+from typing import Any, Dict, Mapping, Sequence
+
+from ai_genomics.utils.text_embedding import embed
+
+
+def embed_entities(
+    entities: Mapping[str, Sequence[str]],
+    model: str,
+) -> pd.DataFrame:
+    """_summary_
+
+    Args:
+        entities (Mapping[str, Mapping[str, Union[str, str]]]): Entities
+            (without scores) for a set of documents.
+        model (str): Name of a sentence transformer model.
+
+    Returns:
+        pd.DataFrame: Dataframe where rows are entities and columns are
+            embedding values.
+    """
+    entities_unique = list(set(chain(*entities.values())))
+    embeddings = embed(entities_unique, model)
+
+    return pd.DataFrame(embeddings, index=entities_unique)
+
+
+def create_entity_embedding_clusters(
+    entity_embeddings: pd.DataFrame,
+    cluster_params: Mapping[str, Any],
+) -> Dict[str, int]:
+    """Clusters entity embeddings using K means and returns a lookup between
+    entities and their cluster ID.
+
+    Args:
+        entity_embeddings (pd.DataFrame): Embeddings for a set of entities.
+        cluster_params (Mapping[str, Any]): A dictionary or other mapping of
+            kwargs to pass into the clusterer.
+
+    Returns:
+        Dict[str, int]: A lookup between entities and their cluster IDs.
+    """
+    km = KMeans(**cluster_params)
+    km.fit(entity_embeddings)
+
+    return dict(zip(entity_embeddings.index, [int(l) for l in km.labels_]))
+
+
+def create_doc_vectors(
+    entities: Mapping[str, Sequence[str]],
+    entity_to_clusters_lookup: Mapping[str, int],
+) -> pd.DataFrame:
+    """Create document vectors that represent the presence of an entity cluster
+    within a document.
+
+
+
+    Args:
+        entities (Mapping[str, Sequence[str]]): Entities (without scores) for
+            a set of documents.
+        entity_to_clusters_lookup (Mapping[str, int]): A lookup between
+            entities and their cluster.
+
+    Returns:
+        pd.DataFrame: The document vectors.
+    """
+
+    d = max(entity_to_clusters_lookup.values())
+
+    doc_vectors = []
+    for ents in entities.values():
+        entity_groups = [
+            entity_to_clusters_lookup[e]
+            for e in ents
+            if entity_to_clusters_lookup.get(e) is not None
+        ]
+        doc_vectors.append(np.np.bincount(entity_groups, minlength=d))
+
+    return pd.DataFrame(np.array(doc_vectors), index=entities.keys())
---
+++ ai_genomics/pipeline/entity_cluster/create_entity_clusters.py
@@ -0,0 +1,84 @@
+import json
+from toolz.dicttoolz import merge
+from toolz.functoolz import pipe
+
+from ai_genomics import PROJECT_DIR, get_yaml_config, logger
+from ai_genomics.utils.reading import make_path_if_not_exist
+
+from ai_genomics.getters.openalex import (
+    get_openalex_entities,
+    get_openalex_ai_genomics_works,
+)
+from ai_genomics.getters.patents import (
+    get_ai_genomics_patents_entities,
+    get_ai_genomics_patents,
+)
+from ai_genomics.getters.gtr import get_gtr_entities, get_ai_genomics_project_table
+from ai_genomics.getters.crunchbase import (
+    get_crunchbase_entities,
+    get_ai_genomics_crunchbase_orgs,
+)
+
+from ai_genomics.pipeline.entity_cluster import (
+    embed_entities,
+    create_entity_embedding_clusters,
+)
+from ai_genomics.utils.entities import (
+    filter_entities,
+    strip_scores,
+)
+
+
+CONFIG = get_yaml_config(PROJECT_DIR / "ai_genomics/config/entity_cluster.yaml")
+OUT_DIR = PROJECT_DIR / "inputs/entities/"
+
+
+if __name__ == "__main__":
+
+    logger.info("Fetching and merging entities.")
+    gtr_ids = list(get_ai_genomics_project_table().query("ai_genomics == True")["id"])
+    oa_ids = list(
+        get_openalex_ai_genomics_works().query("ai_genomics == True")["work_id"]
+    )
+    cb_ids = list(get_ai_genomics_crunchbase_orgs().query("ai_genom == True")["id"])
+
+    oa_entities = get_openalex_entities()
+    oa_entities = {k: oa_entities[k] for k in oa_ids}
+
+    gtr_entities = get_gtr_entities()
+    gtr_entities = {k: gtr_entities[k] for k in gtr_ids}
+
+    cb_entities = get_crunchbase_entities()
+    cb_entities = {k: cb_entities[k] for k in cb_ids}
+
+    patent_entities = get_ai_genomics_patents_entities()
+
+    entities = merge(
+        oa_entities,
+        gtr_entities,
+        cb_entities,
+        patent_entities,
+    )
+
+    logger.info("Filtering and embedding entities.")
+    entities = strip_scores(entities)
+    entities = filter_entities(
+        entities,
+        **CONFIG["filter_entities"],
+    )
+
+    embeddings = embed_entities(
+        entities,
+        **CONFIG["embed"],
+    )
+
+    make_path_if_not_exist(OUT_DIR)
+    for k, params in CONFIG["cluster"].items():
+        logger.info(f"Clustering {k} entities.")
+        cluster_lookup = create_entity_embedding_clusters(
+            embeddings,
+            params,
+        )
+
+        with open(OUT_DIR / f"entity_groups_{k}.json", "w") as f:
+            json.dump(cluster_lookup, f)
