86abf447b3e3ce0b8cdfe1b598422191cce949bf
George Richardson <georgerichardson@posteo.net>
2022-10-18 14:57:31 +0100

    Add entity cluster getter

---
+++ ai_genomics/getters/entities.py
@@ -0,0 +1,18 @@
+from typing import Dict
+
+from ai_genomics import bucket_name
+from ai_genomics.getters.data_getters import load_s3_data
+
+
+def get_entity_cluster_lookup(k: int = 500) -> Dict[str, int]:
+    """Gets a lookup between DBpedia entities and their entity cluster IDs.
+
+    Args:
+        k (int, optional): The number of clusters. Defaults to 500.
+
+    Returns:
+        Dict[str, int]: A lookup where keys are entity names and values are
+            cluster IDs
+    """
+    fname = f"inputs/entities/entity_groups_k_{k}.json"
+    load_s3_data(bucket_name, fname)
