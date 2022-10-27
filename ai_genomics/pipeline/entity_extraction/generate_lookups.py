da07da740f6144a0f763864b93abb724dc7caf98
India Kerle <india.kerle@nesta.org.uk>
2022-10-27 15:19:11 +0200

    removed sys prettier

--- ai_genomics/pipeline/entity_extraction/generate_lookups.py
+++ ai_genomics/pipeline/entity_extraction/generate_lookups.py
@@ -7,12 +7,26 @@ import pandas as pd
 import os
 from typing import Dict, List
 
-from ai_genomics import bucket_name
+from ai_genomics import PROJECT_DIR, bucket_name, logger
 from ai_genomics.getters.data_getters import load_s3_data, save_to_s3
 from ai_genomics.getters.patents import (
     get_ai_sample_patents,
     get_genomics_sample_patents,
     get_ai_genomics_patents,
+)
+
+import boto3
+from boto3.s3.transfer import TransferConfig
+import json
+
+# to deal with OA size now
+S3 = boto3.client("s3")
+# add config to allow for larger multi_part threshold
+config = TransferConfig(
+    multipart_threshold=1024 * 25,
+    max_concurrency=10,
+    multipart_chunksize=1024 * 25,
+    use_threads=True,
 )
 
 CB_DATA = load_s3_data(bucket_name, "outputs/crunchbase/crunchbase_ai_genom_comps.csv")
@@ -26,6 +40,7 @@ AI_PATENTS, GENOMICS_PATENTS, AI_GENOMICS_PATENTS = (
     get_ai_genomics_patents(),
 )
 
+OA_LOOKUP_LOCAL_PATH = str(PROJECT_DIR) + "/outputs/data/openalex/oa_lookup.json"
 LOOKUP_TABLE_PATH = "inputs/lookup_tables/"
 VALID_DF_TYPES = ["ai", "genomics", "ai_genomics"]
 
@@ -50,9 +65,8 @@ if __name__ == "__main__":
         ), "not every id is unique"
         patent_lookup = (
             lookup.query("abstract_language == 'en'")
-            .dropna(subset=["abstract_text"])
-            [['publication_number', 'abstract_text']]
-            .rename(columns={'publication_number': 'id', 'abstract_text': 'abstract'})
+            .dropna(subset=["abstract_text"])[["publication_number", "abstract_text"]]
+            .rename(columns={"publication_number": "id", "abstract_text": "abstract"})
             .T.to_dict()
         )
         patent_lookups.extend(list(patent_lookup.values()))
@@ -60,14 +74,24 @@ if __name__ == "__main__":
 
     # generate and save cb look ups across ai + genomics, ai baseline and genomics baseline
     assert pd.Series(CB_DATA.id).is_unique == True, "not every id is unique"
-    cb_lookup = list(CB_DATA[['id', 'description_combined']].rename(columns={'description_combined': 'abstract'}).T.to_dict().values())
+    cb_lookup = list(
+        CB_DATA[["id", "description_combined"]]
+        .rename(columns={"description_combined": "abstract"})
+        .T.to_dict()
+        .values()
+    )
     save_to_s3(
         bucket_name, cb_lookup, os.path.join(LOOKUP_TABLE_PATH, "cb_lookup.json")
     )
 
     # generate and save gtr look ups across ai + genomics, ai baseline and genomics baseline gtr
     assert pd.Series(GTR_DATA.id).is_unique == True, "not every id is unique"
-    gtr_lookup = list(GTR_DATA[['id', 'abstract_text']].rename(columns={'abstract_text': 'abstract'}).T.to_dict().values())
+    gtr_lookup = list(
+        GTR_DATA[["id", "abstract_text"]]
+        .rename(columns={"abstract_text": "abstract"})
+        .T.to_dict()
+        .values()
+    )
     save_to_s3(
         bucket_name, gtr_lookup, os.path.join(LOOKUP_TABLE_PATH, "gtr_lookup.json")
     )
@@ -103,12 +127,20 @@ if __name__ == "__main__":
     oa_abstracts_clean = {}
     for id_, abstract in OA_ABSTRACTS.items():
         if abstract not in bad_abstracts:
-            oa_abstracts_clean['id'] = id_
-            oa_abstracts_clean['abstract'] = abstract
+            oa_abstracts_clean["id"] = id_
+            oa_abstracts_clean["abstract"] = abstract
         oa_abstracts_clean_list.append(oa_abstracts_clean)
 
-    save_to_s3(
+    with open(OA_LOOKUP_LOCAL_PATH, "w") as f:
+        json.dump(oa_abstracts_clean_list, f)
+
+    logger.info("loaded oa_lookup to file.")
+
+    # Upload to bucket-name at key-name with config
+    S3.upload_file(
+        OA_LOOKUP_LOCAL_PATH,
         bucket_name,
-        oa_abstracts_clean_list,
-        os.path.join(LOOKUP_TABLE_PATH, "oa_lookup.json"),
+        LOOKUP_TABLE_PATH + "oa_lookup.json",
+        Config=config,
     )
+    logger.info("uploaded oa_lookup to s3")
