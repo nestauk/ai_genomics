5ab692bdf74d95d4ac0e76a170e91e609a9cfd44
George Richardson <georgerichardson@posteo.net>
2022-11-03 16:41:46 +0000

    162 cluster entity dist (#163)
    * Add getter for cluster names
    
    * Update default year range

--- ai_genomics/getters/clusters.py
+++ ai_genomics/getters/clusters.py
@@ -7,7 +7,7 @@ from ai_genomics.getters.data_getters import load_s3_data, get_s3_dir_files
 
 def get_doc_cluster_lookup(
     ai_only: bool = False,
-    min_year: int = 2020,
+    min_year: int = 2012,
     max_year: int = 2021,
 ) -> Dict[str, Dict[int, str]]:
     """Get patent and OpenAlex document clusters.
@@ -39,3 +39,26 @@ def get_doc_cluster_lookup(
                 print(f)
         else:
             raise
+
+
+def get_doc_cluster_names(
+    ai_only: bool = False,
+    min_year: int = 2012,
+    max_year: int = 2021,
+) -> Dict[int, str]:
+    """Get document cluster names generated through tf-idf of entity counts.
+
+    Args:
+        ai_only (bool, optional): If True, loads clusters for a narrow subset of
+            AI papers only. Defaults to False.
+        min_year (int): Load clusters with docs from, and including, this year.
+        max_year (int): Load clusters with docs up to, and including, this year.
+
+    Returns:
+        Dict[int, str]: Lookup between cluster ID and document names. (Top 25 tf-idf terms)
+    """
+    subset = "ai" if ai_only else "all"
+    fname = (
+        f"outputs/data/cluster/doc_{subset}_{min_year}_{max_year}_clusters_names.json"
+    )
+    return load_s3_data(bucket_name, fname)
